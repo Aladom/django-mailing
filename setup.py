086027562378bfde47bba38d6da340a5f58ab3cd
Antoine Pinsard <antoine.pinsard@gmail.com>
2016-01-30 14:29:38 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-mailing',
+    version='0.1',
+    packages=['mailing'],
+    include_package_data=True,
+    license='MIT',
+    description='Django app to template and queue e-mails.',
+    long_description=README,
+    url='https://github.com/Aladom/django-mailing',
+    author="Aladom & Cocoonr",
+    author_email='contact@aladom.fr, contact@cocoonr.fr',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
