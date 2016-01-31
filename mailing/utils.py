bf2cca1f98976e350eb80f14cfafb8796c088f50
Antoine Pinsard <antoine.pinsard@cocoonr.fr>
2016-01-31 11:30:54 +0100

    Added 'queue_mail' utility

--- mailing/utils.py
+++ mailing/utils.py
@@ -3,23 +3,32 @@
 from django.template import Template
 
 from .conf import SUBJECT_PREFIX
+from .models import Mail
 
-def render_mail(campaign, context):
-    subject = Template(self.subject).render(context)
-    if self.prefix_subject and SUBJECT_PREFIX:
+
+def render_mail(campaign, context={}):
+    subject = Template(campaign.subject).render(context)
+    if campaign.prefix_subject and SUBJECT_PREFIX:
         subject = '{} {}'.format(SUBJECT_PREFIX, subject)
 
     try:
-        with open(self.template_file, 'r') as f:
+        with open(campaign.template_file, 'r') as f:
             html_body = Template(f.read()).render(context)
     except:
         pass  # TODO
 
-    mail = Mail(campaign=self, subject=subject, html_body=html_body)
+    mail = Mail(campaign=campaign, subject=subject, html_body=html_body)
 
-    for header in self.additional_headers.all():
+    for header in campaign.additional_headers.all():
         mail.headers.add(
             name=header['name'],
             value=Template(header['value']).render(context))
 
+    return mail
+
+
+def queue_mail(campaign_key, context):
+    campaign = Campaign.objects.get(key=campaign_key)
+    mail = render_mail(campaign, context)
+    mail.save()
     return mail
