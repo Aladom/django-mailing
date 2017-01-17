e773d153ec92bf007e91d68502e3fb917b6ec59b
Arnaud Bouillod <arnaud.bouillod@aladom.fr>
2017-01-17 17:15:18 +0100

    purge_old_mail script updated #126

--- mailing/management/commands/purge_old_mails.py
+++ mailing/management/commands/purge_old_mails.py
@@ -2,53 +2,46 @@
 from datetime import date, timedelta
 
 from django.core.management.base import BaseCommand
+
 from ...models import Mail
 
 
 class Command(BaseCommand):
-    help = """Purge old sended mails."""
+    help = """Purge mails past given period."""
 
     def add_arguments(self, parser):
         parser.add_argument(
             'days', type=int,
-            help="Specify the day frequency.")
+            help="Number of days to keep archived now.")
         parser.add_argument(
-            '-e', '--exclude-statuses', nargs='*', type=str,
-            help="Exclude specified statuses.")
+            '-e', '--exclude-statuses', nargs='*', default=[], type=str,
+            help="""Prevent deletion of mails at the given statuses."""
+                 """ This can be either statuses uppercased name of"""
+                 """ integer values""")
         parser.add_argument(
-            '-o', '--only-statuses', nargs='*', type=str,
-            help="Delete mail with specified statuses.")
+            '-o', '--only-statuses', nargs='*', default=[], type=str,
+            help="""Only delete mails at the given statuses. This can be"""
+                 """ either statuses uppercased name or integer value""")
 
     def handle(self, *args, **options):
-        exclude_statuses = set()
-        only_statuses = set()
         statuses = {}
-        for x in dir(Mail):
-            if x.startswith("STATUS_") and x != "STATUS_CHOICES":
-                statuses[x[7:]] = getattr(Mail, x)
-
-        for status in options['exclude_statuses']:
-            if status in statuses:
-                exclude_statuses.add(statuses[status])
-            elif status.isdigit():
-                exclude_statuses.add(int(status))
-            else:
-                raise ValueError("{} is not a valid status".format(status))
-        for status in options['only_statuses']:
-            if status in statuses:
-                only_statuses.add(statuses[status])
-            elif status.isdigit():
-                only_statuses.add(int(status))
-            else:
-                raise ValueError("{} is not a valid status".format(status))
-        end = date.today()
-        start = end - timedelta(days=options['days'])
-        mails = Mail.objects.filter(
-           scheduled_on__date__gte=start,
-           scheduled_on__date__lte=end
-        )
-        if only_statuses:
-            mails = mails.filter(status__in=only_statuses)
-        if exclude_statuses:
-            mails = mails.exclude(status__in=exclude_statuses)
+        for prop in dir(Mail):
+            if prop.startswith("STATUS_") and prop != "STATUS_CHOICES":
+                statuses[prop[7:]] = getattr(Mail, prop)
+        new_options = {}
+        for opt in ('only_statuses', 'exclude_statuses'):
+            new_options[opt] = set()
+            for status in options[opt]:
+                if status in statuses:
+                    new_options[opt].add(statuses[status])
+                elif status.isdigit():
+                    new_options[opt].add(int(status))
+                else:
+                    raise ValueError("{} is not a valid status".format(status))
+        delete_until = date.today() - timedelta(days=options['days'])
+        mails = Mail.objects.filter(scheduled_on__date__lt=delete_until)
+        if new_options['only_statuses']:
+            mails = mails.filter(status__in=new_options['only_statuses'])
+        if new_options['exclude_statuses']:
+            mails = mails.exclude(status__in=new_options['exclude_statuses'])
         mails.delete()
